258a4381a01b754a0e8ea5bb25ee8ee310f4d742
Keen <agurokeendavid@gmail.com>
2020-01-28 16:32:30 +0800

    Added first lesson.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This add the phrase Hello World! to the console window.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
