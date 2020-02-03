25a4358318e7e3b1210c227ee59d695c6d7d5125
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-03 16:11:25 +0800

    Added Value vs Reference Lecture.

---
+++ ValueVsReferenceTypesLecture2/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueVsReferenceTypesLecture2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray1 = { 1, 2, 3 };
+
+            someMethod(myArray1);
+            Console.WriteLine(myArray1[0]);
+
+            myArray1[0] = 1;
+
+            someMethod2(myArray1);
+            Console.WriteLine(myArray1[0]);
+
+            someMethod3(ref myArray1);
+            Console.WriteLine(myArray1[0]);
+
+            Console.ReadKey();
+        }
+
+        static void someMethod(int[] myArray2)
+        {
+            myArray2[0] = 7;
+        }
+
+        static void someMethod2(int[] myArray2)
+        {
+            myArray2 = new int[3];
+        }
+
+        static void someMethod3(ref int[] myArray2)
+        {
+            myArray2 = new int[3];
+        }
+    }
+}
