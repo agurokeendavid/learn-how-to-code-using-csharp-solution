789fbf8e5df8edca88f01643b9563541a7fa6540
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-04 15:30:46 +0800

    Added "IntroToOOP" lesson.

---
+++ IntroToOOPLecture3/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntroToOOPLecture3
+{
+    internal class Car
+    {
+        public string color;
+        public string brand;
+        public int currentSpeed;
+
+        public void Accelerate(int speedIncrease)
+        {
+            currentSpeed += speedIncrease;
+        }
+
+        public void Decelerate(int speedDecrease)
+        {
+            currentSpeed -= speedDecrease;
+        }
+
+        public void DisplaySpeed()
+        {
+            Console.WriteLine($"The {color} {brand} is travelling at {currentSpeed} km/h");
+        }
+    }
+}
---
+++ IntroToOOPLecture3/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyNamespace;
+namespace IntroToOOPLecture3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            Car Car2 = new Car();
+
+            ExistingCode.Car Car3 = new ExistingCode.Car();
+
+            Lamp Lamp1 = new Lamp();
+            Car1.color = "red";
+            Car1.brand = "Toyota";
+            Car1.currentSpeed = 0;
+
+            Car2.color = "green";
+            Car2.brand = "Honda";
+            Car2.currentSpeed = 0;
+
+            Car1.Accelerate(20);
+        }
+    }
+}
