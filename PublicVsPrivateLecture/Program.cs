80d8d7d3b0e0a03cf02edb7eaadc404b88305941
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-05 10:08:13 +0800

    Added "Public vs private lecture"

---
+++ PublicVsPrivateLecture/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicVsPrivateLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.SetColor("red");
+            car1.SetBrand("Toyota");
+
+            var car2 = new Car("green", "Honda");
+
+            car1.Accelerate(20);
+            car2.Accelerate(20);
+            car2.Decelerate(10);
+
+            car1.DisplaySpeed();
+            car2.DisplaySpeed();
+
+            Console.ReadKey();
+
+        }
+    }
+}
