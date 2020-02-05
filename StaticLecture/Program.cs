10b47d40f6cde92f33190e3f781db7d1e16522ff
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-05 10:29:55 +0800

    Added "Static Lecture"

---
+++ StaticLecture/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Car.GetNumCarObjects());
+
+            var car1 = new Car("red", "Toyota");
+            Console.WriteLine(Car.GetNumCarObjects());
+
+            var car2 = new Car("green", "honda");
+            Console.WriteLine(Car.GetNumCarObjects());
+
+            var car3 = new Car();
+            Console.WriteLine(Car.GetNumCarObjects());
+
+            Console.ReadKey();
+        }
+    }
+}
