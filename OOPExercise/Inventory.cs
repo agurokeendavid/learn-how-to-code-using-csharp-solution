4aa882bc9c6b9a8bb390813d7a4831dabb88523e
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-07 16:05:15 +0800

    Added OOP Lecture.

---
+++ OOPExercise/Inventory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPExercise
+{
+    static class Inventory
+    {
+        private static readonly List<Product> Products;
+
+        static Inventory()
+        {
+            Products = new List<Product>();
+        }
+
+        public static void AddProduct(Product p)
+        {
+            Products.Add(p);
+        }
+
+        public static void DisplayInventory()
+        {
+            double totalInventoryValue = 0;
+            Console.WriteLine("Current inventory: ");
+            Console.WriteLine("===========================================\n");
+            foreach (Product p in Products)
+            {
+                p.DisplayInfo();
+                Console.WriteLine();
+                totalInventoryValue += p.GetTotalValue();
+            }
+
+            Console.WriteLine($"Total inventory value: ${totalInventoryValue:F}");
+        }
+    }
+}
