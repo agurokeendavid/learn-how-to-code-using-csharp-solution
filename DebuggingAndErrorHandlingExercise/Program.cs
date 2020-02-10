7199a00db174bb32222f3b07a704b7d732fd6c57
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-10 10:22:00 +0800

    Added "Debugging and Error Handling Exercise"

---
+++ DebuggingAndErrorHandlingExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingAndErrorHandlingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] sales = new double[4];
+            double totalSales = 0;
+            double totalExpenses;
+            double profit;
+            double averageProfitPerDistrict;
+
+            // input sales from each of the four districts
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write($"Enter sales from district {i + 1}: ");
+                sales[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.Write($"Enter total expenses: ");
+            totalExpenses = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // calculate total sales
+            for (int i = 0; i <= 3; i++)
+            {
+                totalSales += sales[i];
+            }
+            // calculate profit
+            profit = totalSales - totalExpenses;
+
+            // calculate average profit per district
+            averageProfitPerDistrict = profit / 4;
+
+            Console.WriteLine($"Average profit per district was ${averageProfitPerDistrict.ToString("F")}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DebuggingAndErrorHandlingExercise2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingAndErrorHandlingExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte selection;
+            bool wasError;
+
+            do
+            {
+                wasError = false;
+                Console.WriteLine("Please make a selection to learn more information");
+                Console.WriteLine("1 - Tyrannosaurus rex");
+                Console.WriteLine("2 - Stegosaurus");
+                Console.WriteLine("3 - Velociraptor");
+                try
+                {
+                    selection = Convert.ToByte(Console.ReadLine());
+                    Console.WriteLine();
+
+                    switch (selection)
+                    {
+                        case 1:
+                            DisplayTyranosaurusRexInfo();
+                            break;
+                        case 2:
+                            DisplayStegosaurusInfo();
+                            break;
+                        case 3:
+                            DisplayVelociraptorInfo();
+                            break;;
+                        default:
+                                throw new Exception($"{selection} is not one of the choices.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                    wasError = true;
+                }
+            } while (wasError);
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayVelociraptorInfo()
+        {
+            Console.WriteLine("Velociraptor, meaning is a tyrant lizard, from the Ancient Greek veloci, and raptor.");
+        }
+
+        private static void DisplayStegosaurusInfo()
+        {
+            Console.WriteLine("Stegosaurs, meaning is a tyrant lizard, from the Ancient Greek stego, and sauros.");
+        }
+
+        static void DisplayTyranosaurusRexInfo()
+        {
+            Console.WriteLine("Tyrannosaurus, meaning is a tyrant lizard, from the Ancient Greek tyrannos, and sauros.");
+        }
+    }
+}
