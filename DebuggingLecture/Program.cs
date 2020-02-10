1db72a86ec72fc8dc92d278b64d0f704860bc721
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-10 09:25:59 +0800

    Added "Debugging Lecture"

---
+++ DebuggingLecture/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger = 5;
+            
+            var car1 = new Car("red", "Toyota");
+
+            myInteger = 7;
+
+            car1.Accelerate(20);
+            car1.Decelerate(10);
+
+            Console.WriteLine(myInteger);
+
+            car1.Accelerate(20);
+            car1.Decelerate(10);
+
+            if (myInteger > 5)
+                Console.WriteLine(myInteger);
+
+            Console.ReadKey();
+        }
+    }
+}
