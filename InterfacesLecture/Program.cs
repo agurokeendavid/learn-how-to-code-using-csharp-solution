70df4b60c55fd357008caffbee89c1df7efe8e41
Keen David Aguro <agurokeendavid@gmail.com>
2020-02-10 11:37:27 +0800

    Added "Interface Lecture"

--- InterfacesLecture/Program.cs
+++ InterfacesLecture/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace InterfacesLecture
 {
+    interface IMyInterface
+    {
+        void SomeMethod();
+        int AnotherMethod(string someString);
+    }
+
+    class MyClass : IMyInterface
+    {
+        private int x, y;
+
+        public void MyMethod()
+        {
+            // method code
+        }
+
+        public void SomeMethod()
+        {
+            // Method code
+        }
+
+        public int AnotherMethod(string myString)
+        {
+            // method code
+            return 1;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            IMyInterface myInterface;
+            var myClassObject = new MyClass();
+
+            myInterface = myClassObject;
+
+            myInterface.SomeMethod();
+            myInterface.AnotherMethod("hello");
+            Console.ReadKey();
         }
     }
 }
---
+++ InterfacesLecture3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesLecture3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void PrintObject(IDisplay displayObject)
+        {
+            Console.WriteLine("Here is the object's data:\n");
+            Console.WriteLine("###################################################");
+            Console.WriteLine("*****************************************************\n");
+
+            displayObject.DisplayInfo(); // compiler error - how to do you call a method when you don't know what object will be passed in?
+
+            Console.WriteLine("\n******************************************************");
+            Console.WriteLine("######################################################");
+        }
+    }
+}
